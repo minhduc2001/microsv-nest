e195fd2812955d6326860c399788cf75ba959158
ducnm <ngoduc2468@gmail.com>
2023-10-06 13:55:20 +0700

    remove libs auth

--- libs/authentication/src/authentication.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthenticationService } from './authentication.service';
-
-@Module({
-  providers: [AuthenticationService],
-  exports: [AuthenticationService],
-})
-export class AuthenticationModule {}
